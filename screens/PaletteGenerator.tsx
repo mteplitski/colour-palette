79600576dd4464df11af5adcde9e709b49d696ba
mteplitski <michelle.teplitski@gmail.com>
2021-12-20 14:34:22 -0500

    Add colour table

--- screens/PaletteGenerator.tsx
+++ screens/PaletteGenerator.tsx
@@ -1,15 +1,9 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import { getRandomPalette } from '../util/colour';
-import { Palette } from '../components';
+import ColourTable from './ColourTable';
 
 const INITIAL_PALETTE = getRandomPalette();
-
-const StyledButton = styled.button`
-  background-color: ${(props: any) => props.color};
-  margin-top: 16px;
-  cursor: pointer;
-`;
 
 const PageBackground = styled.div`
   width: 100%;
@@ -38,6 +32,10 @@ const StyledHeading2 = styled.h2`
   color: ${(props: any) => props.color};
 `;
 
+const StyledButton = styled.button`
+  margin-top: 16px;
+`;
+
 const PaletteGenerator = () => {
   const [palette, setPalette] = useState(INITIAL_PALETTE);
   const { colours, light, dark } = palette;
@@ -47,15 +45,22 @@ const PaletteGenerator = () => {
       <PageContainer>
         <StyledHeading color={dark}>Heading</StyledHeading>
         <StyledHeading2 color={dark}>Subheading</StyledHeading2>
-        {/* {colours.map((colour) => (
-          <StyledButton key={colour} color={colour}>
-            {colour}
-          </StyledButton>
-        ))} */}
-        <Palette colours={colours} />
-        <button onClick={() => setPalette(getRandomPalette())}>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+          euismod leo in tellus tincidunt accumsan. Pellentesque convallis
+          ultrices leo elementum bibendum. Vivamus fringilla eu velit ac
+          dignissim. Quisque a risus tincidunt ipsum fringilla tempor. Nulla
+          fringilla magna nec faucibus viverra. Nullam sem massa, malesuada eu
+          egestas in, scelerisque eu lectus. Nulla mattis porta nibh, sed
+          volutpat orci imperdiet a. Nullam sodales ante volutpat laoreet
+          convallis. Maecenas pellentesque pulvinar arcu. Ut sagittis, arcu sit
+          amet ullamcorper sollicitudin, mauris turpis facilisis leo, non
+          sodales risus sem vitae urna. Ut at facilisis nibh, a molestie ante.
+        </p>
+        <ColourTable colours={colours} light={light} dark={dark} />
+        <StyledButton onClick={() => setPalette(getRandomPalette())}>
           New Palette
-        </button>
+        </StyledButton>
       </PageContainer>
     </PageBackground>
   );
